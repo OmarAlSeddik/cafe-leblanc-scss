ae0ddc18032c66c0ce639538f4e7cc39d84fa920
Omar <omaralseddik@gmail.com>
2021-06-29 14:50:32 +0200

    Added theme memory functionality.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-const scrollY = window.pageYOffset;
-
+//Toggle menu functionality.
 const toggleMenu = (toggleQuery, menuId) => {
   const toggle = document.querySelectorAll(toggleQuery);
   const menu = document.getElementById(menuId);
@@ -13,6 +12,10 @@ const toggleMenu = (toggleQuery, menuId) => {
 toggleMenu("#nav-toggle", "nav-menu");
 toggleMenu(".nav__link", "nav-menu");
 
+//Scrolling functionality.
+const scrollY = window.pageYOffset;
+
+//Activates and deactivates links dependending on the Y position.
 const scrollActive = () => {
   const sections = document.querySelectorAll("section[id]");
 
@@ -33,6 +36,7 @@ const scrollActive = () => {
 
 window.addEventListener("scroll", scrollActive);
 
+//Adds the header's box shadow if the Y position is over 100 pixels.
 const scrollHeader = () => {
   const header = document.getElementById("header");
   this.scrollY > 100
@@ -42,6 +46,7 @@ const scrollHeader = () => {
 
 window.addEventListener("scroll", scrollHeader);
 
+//Adds the scroll to top button if the Y position is over 600 pixels.
 const scrollTop = () => {
   const scrollTop = document.getElementById("scroll-top");
   this.scrollY > 600
@@ -50,13 +55,33 @@ const scrollTop = () => {
 };
 
 window.addEventListener("scroll", scrollTop);
-//
-const body = document.body;
+
+//Theme and respective memory functionality.
 const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bx-sun";
 
-const toggleDarkTheme = () => {
-  body.classList.toggle("dark-theme");
-  themeButton.classList.toggle("bx-sun");
-};
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
-themeButton.addEventListener("click", toggleDarkTheme);
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
